e9f85f4c76fcf02ce04fef0148e999793ded5f5d
mzeecob <reshimwe2025@gmail.com>
2020-04-11 14:28:31 +0200

    splash creen and we are done

--- app/src/main/java/com/example/bookapplr/SplashActivity.java
+++ app/src/main/java/com/example/bookapplr/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.bookapplr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,27 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        Thread timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        timer.start();
+
+
     }
 }
